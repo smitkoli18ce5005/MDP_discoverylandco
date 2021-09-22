232c844652501127276557523e7e9d384d81ae3f
Smit Koli <smit.koli@morningstar.com>
2021-09-22 17:05:29 +0530

    Gallery Header Bug Fix

---
+++ src/components/Galleryheader.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="gallery-header">
+        <img src='../assets/Galleryheader.jpg' alt="">
+        <h1>Gallery</h1>
+    </div>
+</template>
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+})
+export default class Galleryheader extends Vue {
+}
+</script>
+<style lang="scss">
+.gallery-header{
+    height: 65vh;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+}
+.gallery-header img{
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+.gallery-header h1{
+    position: relative;
+    z-index: 3;
+    margin-left: 10%;
+    font-weight: 200;
+    font-size: 66px;
+}
+</style>
---
+++ src/views/Gallery.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <Galleryheader />
+  </div>
+</template>
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Galleryheader from '@/components/Galleryheader.vue';
+
+@Options({
+  components: {
+    Galleryheader
+  },
+})
+export default class Gallery extends Vue {}
+</script>
+<style lang="scss">
+</style>
