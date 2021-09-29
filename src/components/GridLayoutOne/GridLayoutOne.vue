61c93b0459b48f4002f8851871a408c150e4148d
Smit Koli <smit.koli@morningstar.com>
2021-09-29 21:20:52 +0530

    [Smit Koli] Made Grid Layout One Component responsive for right align

--- src/components/GridLayoutOne/GridLayoutOne.vue
+++ src/components/GridLayoutOne/GridLayoutOne.vue
@@ -11,7 +11,7 @@
             <img v-if="calledBy == 'Home'" class="div-image1-left" :src="getForHome(picsArray[1])" alt="Image 1">
             <img v-if="calledBy == 'Home'" class="div-image2-left" :src="getForHome(picsArray[0])" alt="Image 2">
             <img v-if="calledBy == 'AboutDLC'" class="div-image1-left" :src="getForAboutDLC(picsArray[0])" alt="Image 1">
-            <img v-if="calledBy == 'AboutDLC'" class="div-image2-left" :src="getForAboutDLC(picsArray[1])" alt="Image 2">
+            <img v-if="calledBy == 'AboutDLC'" class="div-image2-left about-right" :src="getForAboutDLC(picsArray[1])" alt="Image 2">
         </div>
         <div v-else class="welcome-grid-right">
             <div class="welcome-text-right">
