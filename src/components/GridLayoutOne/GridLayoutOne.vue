eb3385baae2e514482f0c385d681d6e5446602c3
Smit Koli <smit.koli@morningstar.com>
2021-09-29 20:57:30 +0530

    [Smit Koli] Made Grid Layout One Component responsive for left align

--- src/components/GridLayoutOne/GridLayoutOne.vue
+++ src/components/GridLayoutOne/GridLayoutOne.vue
@@ -9,9 +9,9 @@
                 </div>
             </div>
             <img v-if="calledBy == 'Home'" class="div-image1-left" :src="getForHome(picsArray[1])" alt="Image 1">
-            <img v-if="calledBy == 'Home'" class="div-image2" :src="getForHome(picsArray[0])" alt="Image 2">
+            <img v-if="calledBy == 'Home'" class="div-image2-left" :src="getForHome(picsArray[0])" alt="Image 2">
             <img v-if="calledBy == 'AboutDLC'" class="div-image1-left" :src="getForAboutDLC(picsArray[0])" alt="Image 1">
-            <img v-if="calledBy == 'AboutDLC'" class="div-image2" :src="getForAboutDLC(picsArray[1])" alt="Image 2">
+            <img v-if="calledBy == 'AboutDLC'" class="div-image2-left" :src="getForAboutDLC(picsArray[1])" alt="Image 2">
         </div>
         <div v-else class="welcome-grid-right">
             <div class="welcome-text-right">
@@ -22,9 +22,9 @@
                 </div>
             </div>
             <img v-if="calledBy == 'Home'" class="div-image1-right" :src="getForHome(picsArray[0])" alt="Image 1">
-            <img v-if="calledBy == 'Home'" class="div-image2" :src="getForHome(picsArray[1])" alt="Image 2">
+            <img v-if="calledBy == 'Home'" class="div-image2-right" :src="getForHome(picsArray[1])" alt="Image 2">
             <img v-if="calledBy == 'AboutDLC'" class="div-image1-right" :src="getForAboutDLC(picsArray[0])" alt="Image 1">
-            <img v-if="calledBy == 'AboutDLC'" class="div-image2" :src="getForAboutDLC(picsArray[1])" alt="Image 2">
+            <img v-if="calledBy == 'AboutDLC'" class="div-image2-right" :src="getForAboutDLC(picsArray[1])" alt="Image 2">
         </div>
     </div>
 </template>
