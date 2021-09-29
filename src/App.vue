99ccc3148360a8eb883375f8e6d88acf3386a522
Smit Koli <smit.koli@morningstar.com>
2021-09-29 22:29:59 +0530

    [Smit Koli] Made Carousel Component responsive

--- src/App.vue
+++ src/App.vue
@@ -23,9 +23,54 @@ body{
 h1, h2, h3, h4{
   font-family: 'Spectral', serif;
   font-weight: 200;
+  font-size: 2.5rem;
 }
 p{
   font-family: 'Work Sans', sans-serif;
   font-weight: 200;
+  font-size: 2.5rem;
 }
+
+
+@media screen and (max-width: 1200px) {   
+}
+
+@media screen and (max-width: 992px) {
+  h3, p{
+            font-size: 2.3rem;
+        }
+        .para-text{
+            font-size: 0.9rem;
+            line-height: 1.5;
+        }
+}
+
+@media screen and (max-width: 768px) {
+  h3, p{
+            font-size: 2rem;
+        }
+        .para-text{
+            font-size: 0.8rem;
+            line-height: 1.4;
+        }
+}
+
+
+@media screen and (max-width: 576px) {
+  h3, p{
+            font-size: 1.8rem;
+        }
+} 
+
+@media screen and (max-width: 480px) {
+  h3, p{
+            font-size: 1.3rem;
+        }
+        .para-text{
+            font-size: 0.55rem;
+        }
+} 
+
+@media screen and (max-width: 320px) {
+} 
 </style>
